775ef614ae7b8d9794291bfb4bcb0ecf2d17a400
kyzak-playz <kyzakplays@gmail.com>
2026-04-13 19:31:17 +0000

    chore: set alembic version table schema to private

--- apps/backend/alembic/env.py
+++ apps/backend/alembic/env.py
@@ -48,6 +48,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        version_table_schema="private"
     )
 
     with context.begin_transaction():
@@ -74,7 +75,11 @@ def run_migrations_online() -> None:
     connectable = create_engine(url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, 
+            target_metadata=target_metadata,
+            version_table_schema="private"
+            )
 
         with context.begin_transaction():
             context.run_migrations()
